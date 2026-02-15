9dc7c982b4e30c3f5621eb62670802d718276cf3
Akhil Pillai <ackledotdev@gmail.com>
2026-02-14 19:52:45 -0800

    height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from 'react';
 export default function HomePage() {
 	return (
 		<>
-			<main className='flex min-h-[calc(100vh-3rem)] flex-col items-center justify-center gap-12 px-12 lg:px-16 xl:px-24'>
+			<main className='flex min-h-[calc(100vh-3rem-20rem)] flex-col items-center justify-center gap-12 px-12 lg:px-16 xl:px-24'>
 				<h1 className='text-3xl'>All Quizzes</h1>
 				<Suspense fallback={<p>Loading quizzes...</p>}>
 					<QuizzesGrid />
